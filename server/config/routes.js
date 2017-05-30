80c94ad4bb9ac156b1b1e07bbed7ee631a23e632
saharAl-shurafat1990 <hayelsahar@gmail.com>
2017-05-30 12:01:32 +0200

    using stripe form to payment

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,27 @@
+var stripe = require('stripe')("sk_test_r2IHx0ZqLtOJ2eeZeZlSSlvG");
 
 module.exports = function (app, express) {
 
+	app.post('/api/charge', function(req, res) {
+    var stripeToken = req.body.stripeToken;
+    console.log(stripeToken)
+    var amount = 1000;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            res.send(204);
+            console.log(charge);
+        }
+    });
+});
 	
+
 };
 
